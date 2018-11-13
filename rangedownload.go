0b5a5b28b75f1db5c09cef9a8d54e67aa2790331
Fernando Correa Neto <fcdoth@gmail.com>
2018-11-13 10:54:49 -0200

    * add code that actually starts consuming the response body and test   for it

--- rangedownload.go
+++ rangedownload.go
@@ -1,11 +1,74 @@
 package rangedownload
 
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+// Rangedownload holds information about a download
 type RangeDownload struct {
-	URL string
+	URL    string
+	client *http.Client
 }
 
+// NewRangeDownload initializes a RangeDownload with the url
 func NewRangeDownload(url string) *RangeDownload {
 	return &RangeDownload{
-		URL: url,
+		URL:    url,
+		client: http.DefaultClient,
+	}
+}
+
+// Start starts downloading the requested URL and sending the read bytes into
+// the out channel
+func (r *RangeDownload) Start(out chan<- []byte, errchn chan<- error) {
+	var read int64
+	// Build the request
+	url, err := url.Parse(r.URL)
+	if err != nil {
+		errchn <- fmt.Errorf("Could not parse URL: %v", r.URL)
+	}
+	req := &http.Request{
+		URL:    url,
+		Method: "GET",
+		Header: make(http.Header),
+	}
+
+	// Perform the request
+	resp, err := r.client.Do(req)
+	if err != nil {
+		errchn <- fmt.Errorf("Could not perform a request to %v", r.URL)
+	}
+	defer resp.Body.Close()
+
+	cl := resp.Header.Get("Content-Length")
+	size, err := strconv.ParseInt(cl, 10, 64)
+	if err != nil {
+		errchn <- fmt.Errorf("Could not parse %v", cl)
+	}
+
+	// Start consuming the response body
+	buf := make([]byte, 4*1024)
+	for {
+		// Read some bytes
+		br, err := resp.Body.Read(buf)
+		if err != nil {
+			// Check for possible end of file indicating end of the download
+			if err.Error() == "EOF" {
+				if size != int64(read) {
+					errchn <- fmt.Errorf("Corrupt downlaod")
+				}
+				break
+			} else {
+				errchn <- fmt.Errorf("Failed reading from response body")
+			}
+		}
+		if br > 0 {
+			// Increment the bytes read and send the buffer out to be written
+			read += int64(br)
+			out <- buf
+		}
 	}
 }
--- rangedownload_test.go
+++ rangedownload_test.go
@@ -1,14 +1,73 @@
 package rangedownload
 
 import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+// Replace Transport and make testing easier
+type RoundTripFunc func(req *http.Request) *http.Response
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+func NewTestClient(fn RoundTripFunc) *http.Client {
+	return &http.Client{
+		Transport: RoundTripFunc(fn),
+	}
+}
 
 func TestNewRangeDownload(t *testing.T) {
 	assert := assert.New(t)
 
 	rangedownload := NewRangeDownload("http://foo.com/some.iso")
 	assert.Equal(rangedownload.URL, "http://foo.com/some.iso")
+}
+
+func TestRangeDownloadStartBadURL(t *testing.T) {
+	assert := assert.New(t)
+
+	var result error
+	rangedownload := NewRangeDownload("123%45%6")
+	out := make(chan []byte, 1)
+	errchn := make(chan error)
+	done := make(chan bool)
+
+	go func() {
+		result = <-errchn
+		done <- true
+	}()
+
+	go rangedownload.Start(out, errchn)
+
+	<-done
+
+	assert.Equal("Could not parse URL: 123%45%6", result.Error())
+
+}
+
+func TestRangeDownloadStartCorrectURL(t *testing.T) {
+	assert := assert.New(t)
+
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		assert.Equal("http://foo.com/some.iso", req.URL.String())
+		resp := &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString("OK")),
+			Header:     make(http.Header),
+		}
+		resp.Header.Set("Content-Length", "2")
+		return resp
+	})
+
+	rangedownload := NewRangeDownload("http://foo.com/some.iso")
+	rangedownload.client = client
+	out := make(chan []byte, 1)
+	errchn := make(chan error, 1)
+	go rangedownload.Start(out, errchn)
 }
