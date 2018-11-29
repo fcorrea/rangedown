83dd0aaeafc35fe590583dea8737c66029c765dd
Fernando Correa Neto <fcdoth@gmail.com>
2018-11-29 15:40:42 -0200

    * checkpoint before all is lost

--- cmd/ranged/main.go
+++ cmd/ranged/main.go
@@ -1,28 +1,62 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/fcorrea/rangedown"
 )
 
-func main() {
-	var url string
-	flag.StringVar(&url, "url", "", "The URL")
+var url string
 
-	flag.Parse()
+func init() {
+	const (
+		URLHelper = "The URL"
+	)
+	flag.StringVar(&url, "url", "", URLHelper)
+	flag.StringVar(&url, "u", "", URLHelper)
+}
 
+func checkArgs(a []string) error {
+	if len(a) == 0 {
+		return errors.New("")
+	}
+	return nil
+}
+
+// Download creates and starts a new download
+func Download(url string) (string, error) {
 	// Create the download
 	download, err := rangedown.NewDownload(url)
 	if err != nil {
-		panic(err.Error())
+		return "", err
 	}
 
 	// Start downloading it
 	download.Start()
 
 	// Wait and check for progress
-	download.Wait()
-	fmt.Println("Download complete.")
+	written, err := download.Wait()
+	if err != nil {
+		panic(err.Error())
+	}
+	return fmt.Sprintf("Downloaded %v successfully. %d bytes written.", download.FileName, written), nil
+}
+
+func main() {
+	flag.Parse()
+
+	err := checkArgs(flag.Args())
+	if err != nil {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	result, err := Download(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(result)
 }
